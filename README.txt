706afa2f67b60d9fb31c399d1d9dc9617599e487
Abbas <SolokiAbbas@gmail.com>
2018-01-15 23:04:09 -0500

    added forms and models

--- README.txt
+++ README.txt
@@ -1,1 +1,7 @@
-My new Project testing the capabilities of Django with Python
+My new Project testing the capabilities of Django with Python.
+
+Coding process:
+1. Bootstrap
+2. Adding URLS to views
+3. Making Models to connect to DATABASES
+4. Making Forms as a GET and POST request
--- myproject/first_app/views.py
+++ myproject/first_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import forms
+
+
 # Create your views here.
+
+def index(request):
+    return render(request, 'first_app/index.html')
+
+def form_name_view(request):
+    form = forms.FormName()
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+        if form.is_valid():
+            print("Success")
+    return render(request, 'first_app/form_page.html', {'form':form})
